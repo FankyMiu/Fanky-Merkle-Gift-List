c58764299cf23902149e5453b6ff6137c2fb7593
fankymiu <miufanky@gmail.com>
2025-07-21 15:44:51 +0800

    feat: initial Merkle Gift List solution

--- client/index.js
+++ client/index.js
@@ -1,14 +1,28 @@
 const axios = require('axios');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
+const name = 'Fanky Miu'; // 此名字需確實存在於 niceList.json！
 
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // 找到名字的 index
+  const index = niceList.findIndex(n => n === name);
 
+  // 若沒找到，報錯並中止
+  if (index === -1) {
+    console.log("名字不在名單裡，請先編輯 niceList.json 加入你的名字！");
+    return;
+  }
+
+  // 產生 Merkle Tree 與 proof
+  const merkleTree = new MerkleTree(niceList);
+  const proof = merkleTree.getProof(index);
+
+  // 呼叫 server，送出 name + proof
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name,
+    proof
   });
 
   console.log({ gift });
--- server/index.js
+++ server/index.js
@@ -1,29 +1,30 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
+const MerkleTree = require('../utils/MerkleTree');
+const niceList = require('../utils/niceList.json');
 
 const port = 1225;
-
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+// 建立 Merkle Tree & 取得 Root
+const merkleTree = new MerkleTree(niceList);
+const MERKLE_ROOT = merkleTree.getRoot();
 
 app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
-  const body = req.body;
+  const { name, proof } = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
-    res.send("You got a toy robot!");
-  }
-  else {
-    res.send("You are not on the list :(");
+  // 驗證 proof
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+
+  if (isInTheList) {
+    res.send({ gift: "You got a toy robot!" });
+  } else {
+    res.send({ gift: "You are not on the list :(" });
   }
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
+  console.log('Merkle Root:', MERKLE_ROOT);
 });
